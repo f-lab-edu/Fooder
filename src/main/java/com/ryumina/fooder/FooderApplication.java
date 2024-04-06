e1e5399b8a6e760718e57fb8262d8caf9baa9c09
Ryumina <alsk464@gmail.com>
2024-04-06 13:11:53 +0900

    first commit

---
+++ src/main/java/com/ryumina/fooder/FooderApplication.java
@@ -0,0 +1,13 @@
+package com.ryumina.fooder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryumina/fooder/FooderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryumina.fooder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
